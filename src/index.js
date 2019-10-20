edac0a2de4f37c3b37c5a8fa21363df1c648a40d
joeltankam <joel.tankam@gmail.com>
2019-10-20 15:18:40 +0100

    Improve help

--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ const program = require('commander');
 const AboutMeLogger = require('./loggers/AboutMeLogger.js');
 
 program
-    .option('-a, --about-me', 'output information about me 👨🏽‍💼')
+    .name('joeltankam')
+    .option('-m, --about-me', 'output information about me 👨🏽‍💼')
     .option('-e, --experience', 'output my professional experience 💻')
     .option('-d, --education', 'output my education 👨‍🎓')
     .option('-s, --skills', 'output my skill highlights 👨‍💻')
     .option('-c, --awards', 'output my awards and certifications 🏆')
     .option('-l, --languages', 'output my language skills 🌍')
     .option('-r, --references', 'output my references 👨‍⚖️')
-    .option('-o, --hobbies', 'output my hobbies 🕺');
+    .option('-o, --hobbies', 'output my hobbies 🕺')
+    .option('-a, --all', 'output everything ⚛');
 
 program.parse(process.argv);
 
 const summaryData = require('../assets/about.json');
 new AboutMeLogger(summaryData, program.aboutMe).log();
+
+console.log();
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
